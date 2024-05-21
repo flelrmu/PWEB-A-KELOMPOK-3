1e69702d15061b7ed3fad612f847dda516103b23
Muhammad Farhan <farhan23102003@gmail.com>
2024-05-21 11:25:13 +0700

    farhan-login-editprofile

--- routes/users.js
+++ routes/users.js
@@ -1,52 +1,56 @@
-var express = require('express');
-const verifyToken = require('../middleware/tokenvalid.js');
-const { editProfile, getUser } = require('../controllers/auth.js');
-const { changePassword } = require('../controllers/auth.js');
+var express = require("express");
+const verifyToken = require("../middleware/tokenvalid.js");
+const { editProfile, getUser } = require("../controllers/auth.js");
+const { changePassword } = require("../controllers/auth.js");
 var router = express.Router();
 
-router.get('/', (req, res) => {
-  res.redirect('/login'); 
+router.get("/", (req, res) => {
+  res.redirect("/login");
 });
 
-router.get("/home", verifyToken('mahasiswa'), async function (req, res) {
-  const user = await getUser(req, res); 
+router.get("/home", verifyToken("mahasiswa"), async function (req, res) {
+  const user = await getUser(req, res);
   res.render("mahasiswa/home", { user });
 });
 
-
-router.get("/profile", verifyToken('mahasiswa'), async function (req, res) {
-const user = await getUser(req, res); 
-res.render("mahasiswa/profile", { user });
+router.get("/profile", verifyToken("mahasiswa"), async function (req, res) {
+  const user = await getUser(req, res);
+  res.render("mahasiswa/profile", { user });
 });
 
+router.get(
+  "/profile/ubahpassword",
+  verifyToken("mahasiswa"),
+  async function (req, res) {
+    const user = await getUser(req, res);
+    res.render("mahasiswa/ubahpassword", { user });
+  }
+);
 
-
-router.get('/profile/ubahpassword',verifyToken('mahasiswa'), async function (req, res) {
-const user = await getUser(req, res); 
-res.render('mahasiswa/ubahpassword', { user });
+router.post("/ubahpassword", verifyToken("mahasiswa"), async (req, res) => {
+  await changePassword(req, res);
 });
 
-router.post('/ubahpassword', verifyToken('mahasiswa'), async (req, res) => {
-await changePassword(req, res);
+router.get("/logout", (req, res) => {
+  res.clearCookie("refreshToken");
+  res.redirect("/login");
 });
 
-
-router.get('/logout', (req, res) => {
-res.clearCookie('refreshToken');
-res.redirect('/login');
+router.post("/ubahpassword", verifyToken, async (req, res) => {
+  await changePassword(req, res);
 });
 
-router.post('/ubahpassword', verifyToken, async (req, res) => {
-await changePassword(req, res);
-});
+router.get(
+  "/profile/editprofil",
+  verifyToken("mahasiswa"),
+  async (req, res) => {
+    const user = await getUser(req, res);
+    res.render("mahasiswa/editprofil", { user });
+  }
+);
 
-router.get('/profile/editprofil',verifyToken('mahasiswa'), async (req, res) => {
-const user = await getUser(req, res); 
-res.render('mahasiswa/editprofil', { user });
-});
-
-router.post('/editprofil', verifyToken('mahasiswa'), async (req, res) => {
-await editProfile(req, res);
+router.post("/editprofil", verifyToken("mahasiswa"), async (req, res) => {
+  await editProfile(req, res);
 });
 
 module.exports = router;
