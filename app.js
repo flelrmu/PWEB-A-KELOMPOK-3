398c4e87ae39871522a65fbf08dcb4d8aea1dd55
ramaghfira <risyda.maghfira305@gmail.com>
2024-06-10 12:39:31 +0700

    menghubungkan halaman login ke dashboard

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require("express");
+const express = require("express");
 const path = require("path");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
@@ -7,44 +7,50 @@ const logger = require('morgan');
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
 
-require('dotenv').config();
+dotenv.config();
 
 const routeIndex = require("./routes/index.js");
 const routeUsers = require("./routes/users.js");
 const routeAdmin = require("./routes/admin.js");
 const routeDosen = require("./routes/dosen.js");
+
 const app = express();
 
-
+// Set view engine
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
 
+<<<<<<< HEAD
 app.use(express.static(path.join(__dirname, 'public')));
 hbs.registerPartials(path.join(__dirname, 'views/partials'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+=======
+// Static files
+const publicDirectory = path.join(__dirname, "./public");
+app.use(express.static(publicDirectory));
+
+// Middleware
+>>>>>>> e5c8093 (menghubungkan halaman login ke dashboard)
 app.use(express.json());
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(cookieParser());
-
-app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
 app.use("/", routeIndex);
 app.use("/", routeUsers);
 app.use('/admin', routeAdmin);
 app.use('/dosen', routeDosen);
 
+// Middleware to check refresh token
 app.use((req, res, next) => {
   const refreshToken = req.cookies.refreshToken;
-  if (refreshToken) {
-    res.locals.userLoggedIn = true;
-  } else {
-    res.locals.userLoggedIn = false;
-  }
+  res.locals.userLoggedIn = !!refreshToken;
   next();
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
