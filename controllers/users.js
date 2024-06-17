5e8638066506533f78fc614c995ec797cc6abda8
Muhammad Farhan <farhan23102003@gmail.com>
2024-06-17 18:05:10 +0700

    daftar-editDaftar-Farhan

--- controllers/users.js
+++ controllers/users.js
@@ -2,55 +2,92 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 const { Users } = require("../models/users.js");
 const { Daftar } = require("../models/pendaftaran.js");
+const path = require('path');
+const fs = require('fs');
 
-const sendForm = async (req, res) => {
-    try {
-      const { inputNama, inputNim, inputTopik, inputJudul, inputDospem } = req.body;
-      
-      const user = await Users.findByPk(req.userId);
-      if (!user) {
-        return res.status(404).json({ message: "Pengguna tidak ditemukan" });
-      }
-  
-      const newForm = await Daftar.create({ 
-        name: inputNama,
-        nim: inputNim,
-        topik: inputTopik,
-        judul: inputJudul,
-        dosenPembimbing: inputDospem
-      });
-  
-      console.log('Form Pendaftaran:', newForm); 
-  
-      return res.redirect('/lihat');
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Terjadi kesalahan server" });
-    }
-  };
+exports.sendForm = async (req, res) => {
+  try {
+    const { inputNama, inputNim, inputTopik, inputJudul, inputDospem } = req.body;
+    const filePath = req.file.path;
 
-  const editDaftar = async (req, res) => {
-    try {
-      const {  inputNama, inputNim, inputTopik, inputJudul, inputDospem } =
-        req.body;
-  
-      const user = await Users.findByPk(req.userId);
-      if (!user) {
-        return res.status(404).json({ message: "Pengguna tidak ditemukan" });
-      }
-  
-      await user.update({
-        name: inputNama,
-        nim: inputNim,
-        topik: inputTopik,
-        judul: inputJudul,
-        dosenPembimbing: inputDospem,
-      });
-      return res.redirect("/lihat");
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({ message: "Terjadi kesalahan server" });
+    const newForm = await Daftar.create({
+      name: inputNama,
+      nim: inputNim,
+      topik: inputTopik,
+      judul: inputJudul,
+      dosenPembimbing: inputDospem,
+      file: filePath,
+    });
+
+    return res.redirect("/lihat/" + newForm.idDaftar);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Terjadi kesalahan server" });
+  }
+};
+
+exports.editDaftar = async (req, res) => {
+  try {
+    const { inputNama, inputNim, inputTopik, inputJudul, inputDospem } = req.body;
+    const daftar = await Daftar.findByPk(req.params.idDaftar);
+
+    if (!daftar) {
+      return res.status(404).json({ message: "Pendaftaran tidak ditemukan" });
     }
-  };
-    
-  module.exports = sendForm;
+
+    const filePath = req.file ? req.file.path : daftar.file;
+
+    await daftar.update({
+      name: inputNama,
+      nim: inputNim,
+      topik: inputTopik,
+      judul: inputJudul,
+      dosenPembimbing: inputDospem,
+      file: filePath,
+    });
+
+    return res.redirect("/lihat/" + daftar.idDaftar);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Terjadi kesalahan server" });
+  }
+};
+
+exports.getForm = (req, res) => {
+  res.render("mahasiswa/daftar");
+};
+
+exports.getDaftar = async (req, res) => {
+  try {
+    const pendaftaran = await Daftar.findByPk(req.params.idDaftar);
+    if (pendaftaran) {
+      res.render('mahasiswa/lihat', {
+        idDaftar: pendaftaran.idDaftar,
+        namaMahasiswa: pendaftaran.name,
+        nimMahasiswa: pendaftaran.nim,
+        topikSeminar: pendaftaran.topik,
+        judul: pendaftaran.judul,
+        dosenPembimbing: pendaftaran.dosenPembimbing,
+        file: pendaftaran.file
+      });
+    } else {
+      res.status(404).send('Pendaftaran tidak ditemukan');
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Terjadi kesalahan server');
+  }
+};
+
+exports.editForm = async (req, res) => {
+  try {
+    const daftar = await Daftar.findByPk(req.params.idDaftar);
+    if (!daftar) {
+      return res.status(404).json({ message: "Pendaftaran tidak ditemukan" });
+    }
+    res.render("mahasiswa/editdaftar", { daftar, idDaftar: req.params.idDaftar });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Terjadi kesalahan server');
+  }
+};
--- middleware/upload.js
+++ middleware/upload.js
@@ -1,14 +1,15 @@
-const multer = require("multer");
-const path = require("path");
+const multer = require('multer');
+const path = require('path');
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, 'public/uploads/'); 
-    },
-    filename: function (req, file, cb) {
-      cb(null, Date.now() + path.extname(file.originalname));
-    }
-  });
-  
-  // Inisialisasi multer dengan konfigurasi penyimpanan
-  const upload = multer({ storage: storage });
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
